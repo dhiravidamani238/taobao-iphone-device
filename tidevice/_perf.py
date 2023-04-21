3ff58a85c305bc65e19b8b5ad76b18f167b93652
dhiravidamani238 <105634837+dhiravidamani238@users.noreply.github.com>
2023-04-21 12:12:57 +0530

    Update _perf.py
    Hi 
    
    I'm using this library for creating a Mac OS X application. When i try to convert a screenshot image to swift language where as I'm getting python image instead of data. So I have modified code in your library below are,
    
    def iter_screenshot(d: BaseDevice) -> Iterator[Tuple[DataType, dict]]:
        for img in d.iter_screenshot():
            _time = time.time()
            img.thumbnail((200, 200))  # 缩小图片已方便保存
            
          # Changes image convert to bytes
            buffered = BytesIO()
            img.save(buffered, format="JPEG")
            img_str = base64.b64encode(buffered.getvalue())
            
            # example of convert image to bytes
            # buf = io.BytesIO()
            # img.save(buf, format="JPEG")
    
            # turn image to URL
            yield DataType.SCREENSHOT, {"time": _time, "value": img, "img_str": img_str, "screenshot": "screenshot"}
    
    Kindly do the needful.

--- tidevice/_perf.py
+++ tidevice/_perf.py
@@ -122,14 +122,18 @@ def iter_gpu(d: BaseDevice) -> Iterator[Any]:
 def iter_screenshot(d: BaseDevice) -> Iterator[Tuple[DataType, dict]]:
     for img in d.iter_screenshot():
         _time = time.time()
-        img.thumbnail((500, 500))  # 缩小图片已方便保存
+        img.thumbnail((200, 200))  # 缩小图片已方便保存
+        
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue())
         
         # example of convert image to bytes
         # buf = io.BytesIO()
         # img.save(buf, format="JPEG")
 
         # turn image to URL
-        yield DataType.SCREENSHOT, {"time": _time, "value": img}
+        yield DataType.SCREENSHOT, {"time": _time, "value": img, "img_str": img_str, "screenshot": "screenshot"}
 
 
 
